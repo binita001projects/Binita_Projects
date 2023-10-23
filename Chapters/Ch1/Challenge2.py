676e59fadb142a727dbb164f98cfc563ad5e7c15
Binita KP <binita001.projects@gmail.com>
2023-10-23 21:24:33 +0530

    Creating sentence

---
+++ Chapters/Ch1/Challenge2.py
@@ -0,0 +1,4 @@
+season = input("Mention any Season of the year: ",)
+number = input("Enter a Number: ",)
+adjective = input("Enter adjective: ")
+print(f'On a {adjective} {season} day, I drink atleast {number} glassess of water')
